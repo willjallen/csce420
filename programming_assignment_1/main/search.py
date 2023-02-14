3e13a5b68d11a6b9557a3aa52087ebd74c1c8121
willjallen <wallen2268@gmail.com>
2023-02-13 22:16:13 -0600

    2/3 test cases, simple heuristic works on l1 but hangs on l2 and l3

--- programming_assignment_1/main/search.py
+++ programming_assignment_1/main/search.py
@@ -22,8 +22,7 @@ import util
 import os
 from heapq import *
 import sys
-from dataclasses import dataclass, field
-from typing import Any
+
 def tiny_maze_search(problem):
     """
     Returns a sequence of moves that solves tiny_maze.  For any other maze, the
@@ -63,7 +62,104 @@ def a_really_really_bad_heuristic(position, problem):
 def null_heuristic(state, problem=None):
     return 0
 
-def heuristic1(state, problem=None):
+
+
+
+def manhattan_dist(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
+# https://en.wikipedia.org/wiki/Taxicab_geometry
+def manhattan_dist_heuristic(state, problem):
+        # the state includes a grid of where the food is (problem isn't ter)
+        position, food_grid = state
+        pacman_x, pacman_y = position
+
+        one_fruit = False
+        tiles = food_grid.as_list()
+        if len(tiles) == 0:
+            return 0
+        elif len(tiles) == 1:
+            one_fruit = True
+
+        tiles.sort(key=lambda tile: abs(tile[0]-pacman_x) + abs(tile[1]-pacman_y))
+        # tiles.reverse()
+        # print(len(tiles))
+        closest_fruit = tiles[0]
+        closest_fruit_dist = manhattan_dist(position, closest_fruit) 
+        if one_fruit:
+            return closest_fruit_dist
+
+
+        furthest_fruit = tiles[-1]
+
+        
+        furthest_fruit_dist = manhattan_dist(position, closest_fruit)
+
+        if not one_fruit:
+            second_furthest_fruit = tiles[-2]
+            dist_between_furthest_fruits = manhattan_dist(furthest_fruit, second_furthest_fruit)
+            dist_between_second_furthest_and_player = manhattan_dist(second_furthest_fruit, position) 
+            return dist_between_furthest_fruits + dist_between_second_furthest_and_player
+
+def food_density_heuristic(state, problem):
+        # Heuristic : Density of food in the 4x4 directional region around the player,
+        # extended until it hits a wall. 
+        # Normalize?
+        #
+        foodDensity = 0
+
+        # Up
+        pacman_search_y = pacman_y
+        while(pacman_search_y < food_grid.height):
+            if(food_grid[pacman_x][pacman_search_y]):
+                foodDensity += 1
+            else:
+                break
+            pacman_search_y += 1
+               
+
+        # Down 
+        pacman_search_y = pacman_y
+        while(pacman_search_y >= 0):
+            if(food_grid[pacman_x][pacman_search_y]):
+                foodDensity += 1
+            else:
+                break
+            pacman_search_y -= 1
+               
+
+        # Right
+        pacman_search_x = pacman_x
+        while(pacman_search_x < food_grid.width):
+            if(food_grid[pacman_search_x][pacman_y]):
+                foodDensity += 1
+            else:
+                break
+            pacman_search_x += 1
+               
+        # Left 
+        pacman_search_x = pacman_y
+        while(pacman_search_x >= 0):
+            if(food_grid[pacman_search_x][pacman_y]):
+                foodDensity += 1
+            else:
+                break
+            pacman_search_x -= 1
+
+        # print(foodDensity)
+        return foodDensity
+
+
+def waypoint_heuristic(state, problem):
+    pass
+
+def graph_search_heurisitc():
+    pass
+
+
+
+def heuristic(state, problem=None):
     from search_agents import FoodSearchProblem
     
     # 
@@ -71,35 +167,31 @@ def heuristic1(state, problem=None):
     # 
     if isinstance(problem, SearchProblem):
         # data
+        return 0;
+        print(problem)
         pacman_x, pacman_y = state
         goal_x, goal_y     = problem.goal
         
         # YOUR CODE HERE (set value of optimisitic_number_of_steps_to_goal)
-        
+
         optimisitic_number_of_steps_to_goal = 0
         return optimisitic_number_of_steps_to_goal
     # 
     # traveling-salesman problem (collect multiple food pellets)
     # 
+    # Note: The actual end "goal" of food search is all pieces of food have been eaten
+    # it is not that the closest food has been consumed
     elif isinstance(problem, FoodSearchProblem):
-        # the state includes a grid of where the food is (problem isn't ter)
-        position, food_grid = state
-        pacman_x, pacman_y = position
-        
-        # YOUR CODE HERE (set value of optimisitic_number_of_steps_to_goal)
-        
-        optimisitic_number_of_steps_to_goal = 0
-        return optimisitic_number_of_steps_to_goal
+        return manhattan_dist_heuristic(state, problem)
+    else:
+        return 0 
 
 class Node():
-    def __init__(self, parent, state, action, g, h):
+    def __init__(self, parent, state, action, path_cost):
         self.parent = parent
         self.state = state
         self.action = action
-        self.g = g
-        self.h = h
-        self.path_cost = g + h
-
+        self.path_cost = path_cost 
     def _cmpkey(self):
         return self.path_cost
 
@@ -134,11 +226,12 @@ class AStar():
     def __init__(self, problem, heuristic):
         self.problem = problem
         self.heuristic = heuristic
+        self.precomputed = {}
 
     # Returns solution node, or -1 (failure)
     def search(self):
         start_state = self.problem.get_start_state()
-        rootNode = Node(None, start_state, None, 0, 0)
+        rootNode = Node(None, start_state, None, 0)
         frontier = [] # Heap queue (Priority queue)
         heappush(frontier, rootNode)
         reached = {start_state: rootNode} # Lookup table
@@ -148,9 +241,14 @@ class AStar():
             if(self.problem.is_goal_state(node.state)): return node
             for child in self.problem.get_successors(node.state):
                 s = child.state
-                # h = heuristic1(childNode.state, problem=FoodSearchProblem)
-                childNode = Node(node, s, child.action, node.g + child.cost, 0)
-                if not s in reached.keys() or (childNode.path_cost < reached[s].path_cost):
+                h = heuristic(s, self.problem)
+                # print(h)
+                path_cost = node.path_cost + child.cost + h
+                childNode = Node(node, s, child.action, path_cost)
+                if not s in reached.keys():
+                    reached[s] = childNode
+                    heappush(frontier, childNode)
+                elif childNode.path_cost < reached[s].path_cost:
                     reached[s] = childNode
                     heappush(frontier, childNode)
         return -1
@@ -174,7 +272,7 @@ class AStar():
         return path
 
 
-def a_star_search(problem, heuristic=heuristic1):
+def a_star_search(problem, heuristic=heuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
     
@@ -211,7 +309,7 @@ def a_star_search(problem, heuristic=heuristic1):
    
     astar = AStar(problem, heuristic)
     solution_path = astar.execute()
-    print(solution_path)
+    # print(solution_path)
     return solution_path
 
     # util.raise_not_defined()
